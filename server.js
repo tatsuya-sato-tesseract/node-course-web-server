3eb5aa305135c282a0dc69a10aacb862481c16d9
Tatsuya Sato <tatsuya.g.sato@gmail.com>
2018-11-03 20:03:56 +0900

    made the project page

--- server.js
+++ server.js
@@ -54,6 +54,13 @@ app.get ('/bad', (req, res) => {
     });
 });
 
+app.get ('/projects', (req, res) => {
+    res.render('projects.hbs', {
+        pageTitle: 'Projects',
+        body: "Protfolio page here"
+    })
+})
+
 app.listen(port, () => {
     console.log(`Server is up on port ${port}`);
 });
